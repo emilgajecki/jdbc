16c79fa2f019a8d5e16ecd87053044996203fdbc
emilgajecki <60809250+emilgajecki@users.noreply.github.com>
2020-08-22 10:41:44 +0200

    prosta obsluga przez uzytkownika - polaczenia sql

--- src/main/java/StoreApp.java
+++ src/main/java/StoreApp.java
@@ -1,8 +1,11 @@
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class StoreApp {
+
+    static Scanner scanner = new Scanner(System.in);
 
     // w metodach przekujemy argument połączenia z sql
     static void createStoreTable(Connection connection) throws SQLException {
@@ -18,6 +21,7 @@ public class StoreApp {
                 "");
         create.close();
     }
+
     // metoda wstawiajace dane z odpowiednimi wartosciami
     static void insertRowsIntoStoreTable(Connection connection) throws SQLException {
         Statement insert = connection.createStatement();
@@ -26,15 +30,42 @@ public class StoreApp {
     }
 
     //usuniecie danych z tabeli
-    static void deleteStoreTable (Connection connection) throws SQLException {
+    static void deleteStoreTable(Connection connection) throws SQLException {
         Statement drop = connection.createStatement();
         drop.execute("drop table store ");
         drop.close();
     }
 
+    static int menu() {
+        System.out.print("1. Utwórz tabelę ");
+        System.out.print("2. Dodaj kilka rekordów ");
+        System.out.print("3. Usuń tabele ");
+        System.out.print("0. Koniec ");
+
+        while (!scanner.hasNextInt()) {
+            System.out.println("Wpisz nr polecenia z menu!!!");
+            scanner.nextLine();
+        }
+        return scanner.nextInt();
+    }
+
     public static void main(String[] args) {
 
-
+        while (true){
+            final int option = menu();
+            switch (option){
+                case 1 :
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4 :
+                    break;
+                case 0:
+                    return;
+            }
+        }
     }
 
 }
