569c4bd53185dec4452287da017e3a5752c0ffe1
emilgajecki <60809250+emilgajecki@users.noreply.github.com>
2020-08-22 09:43:46 +0200

    poprawa klasy CreateTable

--- src/main/java/CreateTable.java
+++ src/main/java/CreateTable.java
@@ -11,10 +11,17 @@ public class CreateTable {
 
         //Statement - to jest interfejs
         Statement createTable = connection.createStatement();
-        //tworznie tabeli w sql
+        createTable.execute("drop table kursanci;");
+        //tworznie tabeli w sql - polecenie w sql'u dajemy
         // execute daje true jezeli zwraca jakis wynik,
         // false jezeli nie mamy bledu
         boolean result = createTable.execute("create table kursanci (email varchar(25) primary key, nick varchar(15));");
         System.out.println("Polecenie poprawnie wykonane ?"+result);
+
+        // wstawiamy dane w tablei
+        Statement insertRow = connection.createStatement();
+        int count = insertRow.executeUpdate("insert into kursanci values('karol@op.pl','karole')," +
+                "('ewa@gmail.com','ewa'),('marek@sda.pl','marek');");
+        System.out.println("Liczba wstawionych rekordów: " + count);
     }
 }
