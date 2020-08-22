817b23d0c44afdc4eac1b9c04403b14abb813e90
emilgajecki <60809250+emilgajecki@users.noreply.github.com>
2020-08-22 10:25:57 +0200

    metoda kasowania danych z tablicy,z argumentem polaczenia

--- src/main/java/StoreApp.java
+++ src/main/java/StoreApp.java
@@ -5,7 +5,7 @@ import java.sql.Statement;
 public class StoreApp {
 
     // w metodach przekujemy argument połączenia z sql
-    static void createTable(Connection connection) throws SQLException {
+    static void createStoreTable(Connection connection) throws SQLException {
         // obowiązkowy tworzymy ststement
         Statement create = connection.createStatement();
         create.execute("create table store" +
@@ -16,11 +16,19 @@ public class StoreApp {
                 "voltage decimal (4,1))" +
                 "capacity decimal (4,3));" +
                 "");
+        create.close();
     }
 
-    static void insertRowsIntoStore(Connection connection) throws SQLException {
+    static void insertRowsIntoStoreTable(Connection connection) throws SQLException {
         Statement insert = connection.createStatement();
         insert.executeUpdate("insert into store values (1, 'Jack Daniels', 'whisky', 40.0, 0,7)");
+        insert.close();
+    }
+
+    static void deleteStoreTable (Connection connection) throws SQLException {
+        Statement drop = connection.createStatement();
+        drop.execute("drop table store ");
+        drop.close();
     }
 
 
