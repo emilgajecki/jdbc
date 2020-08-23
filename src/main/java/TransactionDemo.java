d6016ea9390b727c2879076087a21ca563d9a7a3
emilgajecki <60809250+emilgajecki@users.noreply.github.com>
2020-08-23 10:06:48 +0200

    symulacja utracenia połączenia

--- src/main/java/TransactionDemo.java
+++ src/main/java/TransactionDemo.java
@@ -1,45 +1,54 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class TransactionDemo {
 
-    static void createTableAccount(Connection connection) throws SQLException {
-        Statement create = connection.createStatement();
-        create.execute("Create table account ( id integer primary key ,  points integer)");
-        create.executeUpdate("insert into account values (1,100), (2,50)");
-        create.close();
-    }
-    static int getPointsFromAcount(Connection connection, int id) throws SQLException {
-        PreparedStatement select = connection.prepareStatement("select points from account where id=?");
-        select.setInt(1,id);
-        ResultSet set = select.executeQuery();
-        set.next();
-        return set.getInt(1);
-    }
+       static void createTableAccount(Connection connection) throws SQLException {
+            Statement create = connection.createStatement();
+            create.execute("create table account (id integer primary key, points integer);");
+            create.executeUpdate("insert into account values (1, 100), (2, 50);");
+            create.close();
+        }
+        static int getPointsFromAccount(Connection connection, int id) throws SQLException {
+            PreparedStatement select =
+                    connection.prepareStatement("select points from account where id = ?");
+            select.setInt(1, id);
+            ResultSet set = select.executeQuery();
+            set.next();
+            return set.getInt(1);
+        }
+        public static void main(String[] args) throws SQLException {
+            int pointsToTransfer = 20;
+            Connection connection = JdbcConnection.MYSQL_JAVA6.getConnection();
+            //createTableAccount(connection);
+            //Pobieramy z konta o id = 1 liczbę punktów  pointsToTransfer
+            int sourcePoints = getPointsFromAccount(connection, 1);
+            int targetPoints = getPointsFromAccount(connection, 2);
+            System.out.println("Stan konta 1: " + sourcePoints);
+            System.out.println("Stan konta 2: " + targetPoints);
+            PreparedStatement transfer =
+                    connection.prepareStatement("update account set points = ?" +
+                            " where id = ?");
+            transfer.setInt(1, sourcePoints - pointsToTransfer);
+            transfer.setInt(2, 1);
+            transfer.executeUpdate();
+            transfer.close();
+            //Dodajemy do konta o id = 2 liczbę punktów w pointsToTransfer
+            // symulacja przery na łączu
+            Scanner scanner = new Scanner(System.in);
+            scanner.nextLine();
+            PreparedStatement transferTo =
+                    connection.prepareStatement("update account set points = ?" +
+                            " where id = ?");
+            transferTo.setInt(1, pointsToTransfer + targetPoints);
+            transferTo.setInt(2, 2);
+            transferTo.executeUpdate();
+            transferTo.close();
+            sourcePoints = getPointsFromAccount(connection, 1);
+            targetPoints =getPointsFromAccount(connection,2);
+            System.out.println("Stan konta 1 po transferze "+sourcePoints);
+            System.out.println("Stan konta 2 po transferze "+targetPoints);
 
-    public static void main(String[] args) throws SQLException {
-
-        //transfer srodków z jednego konta na drugie
-        //Pobieramy z konta
-        int pointsToTransfer = 20;
-        Connection connection =JdbcConnection.MYSQL_JAVA6.getConnection();
-        //createTableAccount(connection);
-
-        PreparedStatement transfer = connection.prepareStatement("update account set points = " +
-                "(select points - ? from account where id =?) where id =?");
-       transfer.setInt(1,pointsToTransfer);
-       transfer.setInt(2,1);
-       transfer.setInt(3,1);
-       transfer.executeUpdate();
-       transfer.close();
-
-       //dodajemy do drugiego konta
-       PreparedStatement transferTo = connection.prepareStatement("update account set points = " +
-               "(select points + ? from account where id =?) where id =?");
-       transferTo.setInt(1,pointsToTransfer);
-       transferTo.setInt(2,2);
-       transferTo.setInt(3,2);
-       transferTo.executeUpdate();
-       transferTo.close();
     }
 
 }
