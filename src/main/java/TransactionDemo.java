cfaed374c5ff02bb581415b1ab6fc4c0a55cbcc2
emilgajecki <60809250+emilgajecki@users.noreply.github.com>
2020-08-23 09:51:16 +0200

    przelew w konta 1 do 2. Nie dziala na mysql'u - nie pozwala uzyc podzapytania (select points - ? from account where id =?)

--- src/main/java/TransactionDemo.java
+++ src/main/java/TransactionDemo.java
@@ -4,7 +4,8 @@ public class TransactionDemo {
 
     static void createTableAccount(Connection connection) throws SQLException {
         Statement create = connection.createStatement();
-        create.execute("Create table account (int id primary key , int points)");
+        create.execute("Create table account ( id integer primary key ,  points integer)");
+        create.executeUpdate("insert into account values (1,100), (2,50)");
         create.close();
     }
     static int getPointsFromAcount(Connection connection, int id) throws SQLException {
@@ -15,7 +16,30 @@ public class TransactionDemo {
         return set.getInt(1);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
+        //transfer srodków z jednego konta na drugie
+        //Pobieramy z konta
+        int pointsToTransfer = 20;
+        Connection connection =JdbcConnection.MYSQL_JAVA6.getConnection();
+        //createTableAccount(connection);
+
+        PreparedStatement transfer = connection.prepareStatement("update account set points = " +
+                "(select points - ? from account where id =?) where id =?");
+       transfer.setInt(1,pointsToTransfer);
+       transfer.setInt(2,1);
+       transfer.setInt(3,1);
+       transfer.executeUpdate();
+       transfer.close();
+
+       //dodajemy do drugiego konta
+       PreparedStatement transferTo = connection.prepareStatement("update account set points = " +
+               "(select points + ? from account where id =?) where id =?");
+       transferTo.setInt(1,pointsToTransfer);
+       transferTo.setInt(2,2);
+       transferTo.setInt(3,2);
+       transferTo.executeUpdate();
+       transferTo.close();
     }
+
 }
